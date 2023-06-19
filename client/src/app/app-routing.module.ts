282a6a5898eb1e25fe451a431d4fbff4506d94ef
MichelBundel <michel@bundel.eu>
2023-06-19 14:19:13 +0200

    First time 1.02

--- client/src/app/app-routing.module.ts
+++ client/src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import {BudgetBookComponent} from "./budget-book/budget-book.component";
 
-const routes: Routes = [];
+
+
+
+const routes: Routes = [
+  {path:"budgetBook", component:BudgetBookComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
