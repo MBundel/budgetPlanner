263b45eaf142654c729e5996bd35b6693f6eec95
Mirjam <mirjam.kretz@brightstraining.com>
2023-06-20 13:39:47 +0200

    budget-book.component.html: Code eingefügt, Fehler auskommentiert mit Platzhaltern. Component btn erstellt.

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -4,12 +4,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BudgetBookComponent } from './budget-book/budget-book.component';
+import { BtnComponent } from './btn/btn.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
     BudgetBookComponent,
+    BtnComponent,
 
   ],
   imports: [
