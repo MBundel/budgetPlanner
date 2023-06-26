d76bafc5d17cdd3c19b2ba953a0cf73fd638ebdb
JuliaWaller <julia.waller@brightstraining.com>
2023-06-26 11:43:36 +0200

    ResponseEntity hinzugefügt

--- server/src/main/java/com/example/server/user/UserController.java
+++ server/src/main/java/com/example/server/user/UserController.java
@@ -1,6 +1,8 @@
 package com.example.server.user;
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -23,48 +25,48 @@ public class UserController {
     }
 
     @PostMapping("/registration")
-    public RegistrationDTO register(@Valid @RequestBody RegistrationDTO registrationDTO) {
+    public ResponseEntity<RegistrationDTO> register(@Valid @RequestBody RegistrationDTO registrationDTO) {
         RegistrationDTO response = new RegistrationDTO("", "", "", "");
 
         if(registrationDTO.getUsername().equals("")) {
             response.setUsername("This field must not be empty.");
-            return response;
+            return new ResponseEntity<>(response, HttpStatus.NO_CONTENT);
         }
 
         if(!registrationDTO.getPassword1().equals(registrationDTO.getPassword2())) {
             response.setPassword1("The passwords do not match.");
-            return response;
+            return new ResponseEntity<>(response, HttpStatus.NOT_ACCEPTABLE);
         }
 
         if(userService.existsByUsername(registrationDTO.getUsername())) {
             // return new RegistrationDTO("Username is already in use.", "", "", "");
             response.setUsername("Username is already in use.");
-            return response;
+            return new ResponseEntity<>(response, HttpStatus.IM_USED);
         }
 
         if(userService.existsByEmail(registrationDTO.getEmail())) {
             // return new RegistrationDTO("", "", "", "Email address is already in use.");
             response.setEmail("Email address is already in use.");
-            return response;
+            return new ResponseEntity<>(response, HttpStatus.IM_USED);
         }
 
         if(!registrationDTO.getEmail().matches("^[a-zA-Z0-9+_.-]+@[a-zA-Z0-9.-]+$")) {
             // return new RegistrationDTO("", "", "", "This is not a valid email address.");
             response.setEmail("This is not a valid email address.");
-            return response;
+            return new ResponseEntity<>(response, HttpStatus.BAD_REQUEST);
         }
         // TODO: Fehler beheben!
         // "^(?=.[0-9])(?=.[a-z])(?=.[A-Z])(?=.[@#$%^&+=])(?=\\S+$).{8,}$
         // if(!registrationDTO.getPassword1().matches("^(?=.[a-z])(?=.[A-Z])(?=.\\d)(?=.[@$!%?&])[A-Za-z\\d@$!%?&]{8,}$")) {
            if(registrationDTO.getPassword1().length() < 8) {
             // return new RegistrationDTO("", "", "", "This is not a valid password.");
-            response.setPassword1("Your password mus have at least 8 characters.");
-            return response;
+            response.setPassword1("Your password must have at least 8 characters.");
+            return new ResponseEntity<>(response, HttpStatus.FORBIDDEN);
         }
 
         User newUser = userService.addUser(new User(registrationDTO.getUsername(), registrationDTO.getPassword1(), registrationDTO.getEmail()));
         // return new RegistrationDTO(newUser.getUsername(), newUser.getPassword(), newUser.getEmail(), newUser.getRole());
-        return response;
+        return new ResponseEntity<>(response, HttpStatus.OK);
     }
 
     // Für Rest Client und Admin:
