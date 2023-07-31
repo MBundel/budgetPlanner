05727659efb299c74fd2e2048669774996a753b8
MichelBundel <michel@bundel.eu>
2023-07-31 16:54:48 +0200

    refactoring scss

--- client/src/app/sites/home/home.component.ts
+++ client/src/app/sites/home/home.component.ts
@@ -3,7 +3,7 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrls: ['../../styles/forms.scss', '../../styles/general.scss', 'home.scss']
+  styleUrls: ['../../styles/forms.scss', '../../styles/general.scss']
 })
 export class HomeComponent {
 
